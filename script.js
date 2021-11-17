f040e1a7f77cf43849c3d43d6ae429bfabc8ab87
alex <alex.nssbmr@gmail.com>
2021-11-17 17:05:20 +0100

    backdrop

--- script.js
+++ script.js
@@ -11,268 +11,279 @@ let money = 100;
 const BET = 10;
 
 const resetCards = () => {
-  deck = Array.from({ length: 52 * NUMBER_OF_DECKS }, (_, i) => {
-    let newCard = {
-      color:
-        i < (52 * NUMBER_OF_DECKS) / 4
-          ? "hearts"
-          : i < (52 * NUMBER_OF_DECKS) / 2
-          ? "spades"
-          : i < (52 * NUMBER_OF_DECKS) / 2 + (52 * NUMBER_OF_DECKS) / 4
-          ? "diamonds"
-          : "clubs",
-    };
-    if ((i % 13) + 1 === 1) {
-      newCard["symbol"] = "A";
-      newCard["val"] = 11;
-    } else if ((i % 13) + 1 === 11) {
-      newCard["symbol"] = "J";
-      newCard["val"] = 10;
-    } else if ((i % 13) + 1 === 12) {
-      newCard["symbol"] = "Q";
-      newCard["val"] = 10;
-    } else if ((i % 13) + 1 === 13) {
-      newCard["symbol"] = "K";
-      newCard["val"] = 10;
-    } else {
-      //   newCard["symbol"] = "A";
-      newCard["symbol"] = ((i % 13) + 1).toString();
-      //   newCard["val"] = 11;
-      newCard["val"] = (i % 13) + 1;
-    }
-    return newCard;
-  });
+    deck = Array.from({ length: 52 * NUMBER_OF_DECKS }, (_, i) => {
+        let newCard = {
+            color: i < (52 * NUMBER_OF_DECKS) / 4 ?
+                "hearts" : i < (52 * NUMBER_OF_DECKS) / 2 ?
+                "spades" : i < (52 * NUMBER_OF_DECKS) / 2 + (52 * NUMBER_OF_DECKS) / 4 ?
+                "diamonds" : "clubs",
+        };
+        if ((i % 13) + 1 === 1) {
+            newCard["symbol"] = "A";
+            newCard["val"] = 11;
+        } else if ((i % 13) + 1 === 11) {
+            newCard["symbol"] = "J";
+            newCard["val"] = 10;
+        } else if ((i % 13) + 1 === 12) {
+            newCard["symbol"] = "Q";
+            newCard["val"] = 10;
+        } else if ((i % 13) + 1 === 13) {
+            newCard["symbol"] = "K";
+            newCard["val"] = 10;
+        } else {
+            //   newCard["symbol"] = "A";
+            newCard["symbol"] = ((i % 13) + 1).toString();
+            //   newCard["val"] = 11;
+            newCard["val"] = (i % 13) + 1;
+        }
+        return newCard;
+    });
 };
 
 const drawFromDeck = () => {
-  let random = Math.floor(Math.random() * deck.length);
-  let drawnCard = deck[random];
-  deck.splice(random, 1);
-  return drawnCard;
+    // select a random card from the deck, remove it from the deck, and then return the card
+    let random = Math.floor(Math.random() * deck.length);
+    let drawnCard = deck[random];
+    deck.splice(random, 1);
+    return drawnCard;
 };
+const createBackdrop = (titleText, description) => {
+    let backdrop = $("<div>");
+    backdrop.addClass("backdrop");
+    let modal = $("<div>");
+    modal.addClass("modal");
 
+    let title = $("<h1>");
+    title.text(titleText);
+    let desc = $("<p>");
+    desc.text(description);
+
+    let buttonWrapper = $("<div>");
+    buttonWrapper.addClass("playAgainButtons");
+    let noButton = $("<button>");
+    noButton.text("No");
+    noButton.addClass("standbtn")
+    let yesButton = $("<button>");
+    yesButton.text("Yes");
+    yesButton.addClass("hitbtn")
+    buttonWrapper.append(noButton).append(yesButton);
+    modal.append(title).append(desc).append(buttonWrapper);
+    backdrop.append(modal);
+    $("body").prepend(backdrop);
+}
 const createCard = (card) => {
-  let cornerVal = $("<div>");
-  cornerVal.text(card.symbol);
-  cornerVal.addClass(
-    `cornerval ${
-      card.color === "hearts" || card.color === "diamonds" ? "red" : "black"
-    }`
-  );
 
-  let centerVal = $("<div>");
-  centerVal.text(card.symbol);
-  centerVal.addClass(
-    `centerval ${
-      card.color === "hearts" || card.color === "diamonds" ? "red" : "black"
-    }`
-  );
+    let cornerVal = $("<div>");
+    cornerVal.text(card.symbol);
+    cornerVal.addClass(
+        `cornerval ${card.color === "hearts" || card.color === "diamonds" ? "red" : "black"
+        }`
+    );
 
-  let cornerValBottom = $("<div>");
-  cornerValBottom.text(card.symbol);
-  cornerValBottom.addClass(
-    `cornervalbottom ${
-      card.color === "hearts" || card.color === "diamonds" ? "red" : "black"
-    }`
-  );
+    let centerVal = $("<div>");
+    centerVal.text(card.symbol);
+    centerVal.addClass(
+        `centerval ${card.color === "hearts" || card.color === "diamonds" ? "red" : "black"
+        }`
+    );
 
-  let color = $("<div>");
-  color.text(
-    card.color === "hearts"
-      ? "♥"
-      : card.color === "diamonds"
-      ? "♦"
-      : card.color === "spades"
-      ? "♠"
-      : "♣"
-  );
-  color.addClass(
-    `cornerval ${
-      card.color === "hearts" || card.color === "diamonds" ? "red" : "black"
-    }`
-  );
-  let color2 = $("<div>");
-  color2.text(
-    card.color === "hearts"
-      ? "♥"
-      : card.color === "diamonds"
-      ? "♦"
-      : card.color === "spades"
-      ? "♠"
-      : "♣"
-  );
-  color2.addClass(
-    `cornervalbottom ${
-      card.color === "hearts" || card.color === "diamonds" ? "red" : "black"
-    }`
-  );
+    let cornerValBottom = $("<div>");
+    cornerValBottom.text(card.symbol);
+    cornerValBottom.addClass(
+        `cornervalbottom ${card.color === "hearts" || card.color === "diamonds" ? "red" : "black"
+        }`
+    );
 
-  const cardEl = $("<div>")
-    .addClass("card")
-    .append(cornerVal)
-    .append(color)
-    .append(centerVal)
-    .append(color2)
-    .append(cornerValBottom);
-  return cardEl;
+    let color = $("<div>");
+    color.text(
+        card.color === "hearts" ?
+        "♥" :
+        card.color === "diamonds" ?
+        "♦" :
+        card.color === "spades" ?
+        "♠" :
+        "♣"
+    );
+    color.addClass(
+        `cornerval ${card.color === "hearts" || card.color === "diamonds" ? "red" : "black"
+        }`
+    );
+    let color2 = color.clone();
+    color2.addClass(
+        `cornervalbottom ${card.color === "hearts" || card.color === "diamonds" ? "red" : "black"
+        }`
+    );
+
+    const cardEl = $("<div>")
+        .addClass("card")
+        .append(cornerVal)
+        .append(color)
+        .append(centerVal)
+        .append(color2)
+        .append(cornerValBottom);
+
+    return cardEl;
 };
 
 const disableButtons = () => {
-  $("#hit").prop("disabled", true);
-  $("#stand").prop("disabled", true);
+    $("#hit").prop("disabled", true);
+    $("#stand").prop("disabled", true);
 };
 
 const hit = () => {
-  const newCard = drawFromDeck();
-  const card = createCard(newCard);
-  $("#playerCards").append(card);
-  userCards.push(newCard);
-  let acesCount = 0;
+    const newCard = drawFromDeck();
+    const card = createCard(newCard);
+    $("#playerCards").append(card);
 
-  for (const el of userCards) {
-    if (el.symbol === "A") {
-      acesCount++;
+    userCards.push(newCard);
+    let acesCount = 0;
+
+    for (const el of userCards) {
+        if (el.symbol === "A") {
+            acesCount++;
+        }
     }
-  }
 
-  let userScore = [userCards.reduce((a, b) => a + b.val, 0)];
+    let userScore = [userCards.reduce((a, b) => a + b.val, 0)];
 
-  if (userCards.length === 2 && userScore[0] === 21) {
-    $("#userScore").text("Blackjack - You win!");
-    setTimeout(() => {
-      alert("BLACKJACK You win!!");
-      money += 2 * BET;
-      $("#money").text(money);
-    }, 1);
-    disableButtons();
-    return;
-  }
+    if (userCards.length === 2 && userScore[0] === 21) {
+        $("#userScore").text("Blackjack - You win!");
+        setTimeout(() => {
+            $("body").prepend(createBackdrop());
+            money += 2 * BET;
+            $("#money").text(money);
+        }, 1);
+        disableButtons();
+        return;
+    }
 
-  for (let i = 1; i <= acesCount; i++) {
-    userScore.push(userScore[0] - 10 * i);
-  }
+    for (let i = 1; i <= acesCount; i++) {
+        userScore.push(userScore[0] - 10 * i);
+    }
 
-  let scores = userScore.filter((el) => el <= 21);
-  playerScore =
-    scores.length < 1 ? Math.min(...userScore) : Math.max(...scores);
+    let scores = userScore.filter((el) => el <= 21);
 
-  $("#userScore").text(
-    `${scores.length < 1 ? Math.min(...userScore) : scores.join(" or ")}`
-  );
+    playerScore =
+        scores.length < 1 ? Math.min(...userScore) : Math.max(...scores);
 
-  if (Math.min(...userScore) > 21) {
-    setTimeout(() => {
-      alert("You bust");
-      money -= BET;
-      $("#money").text(money);
-    }, 1);
-    disableButtons();
-  }
+    $("#userScore").text(
+        `${scores.length < 1 ? Math.min(...userScore) : scores.join(" or ")}`
+    );
+
+    if (Math.min(...userScore) > 21) {
+        setTimeout(() => {
+            $("body").prepend(createBackdrop("Oh no...", "You bust :( Play again?"));
+
+            money -= BET;
+            $("#money").text(money);
+        }, 1);
+        disableButtons();
+    }
 };
 
 const dealerDraw = (first = false) => {
-  const newCard = drawFromDeck();
-  const card = createCard(newCard);
-  $("#dealerCards").append(card);
-  dealerCards.push(newCard);
-  let acesCount = 0;
+    const newCard = drawFromDeck();
+    const card = createCard(newCard);
+    $("#dealerCards").append(card);
+    dealerCards.push(newCard);
+    let acesCount = 0;
 
-  for (const el of dealerCards) {
-    if (el.symbol === "A") {
-      acesCount++;
+    for (const el of dealerCards) {
+        if (el.symbol === "A") {
+            acesCount++;
+        }
     }
-  }
 
-  let dealerScore = [dealerCards.reduce((a, b) => a + b.val, 0)];
+    let dealerScore = [dealerCards.reduce((a, b) => a + b.val, 0)];
 
-  if (dealerCards.length === 2 && dealerScore[0] === 21) {
-    $("#dealerScore").text("Blackjack - You lose :(");
-    setTimeout(() => {
-      alert("BLACKJACK You lose :9");
-      money -= BET;
-      $("#money").text(money);
-    }, 1);
-    disableButtons();
-    return;
-  }
+    if (dealerCards.length === 2 && dealerScore[0] === 21) {
+        $("#dealerScore").text("Blackjack - You lose :(");
+        setTimeout(() => {
+            alert("BLACKJACK You lose :(");
+            money -= BET;
+            $("#money").text(money);
+        }, 1);
+        disableButtons();
+        return;
+    }
 
-  for (let i = 1; i <= acesCount; i++) {
-    dealerScore.push(dealerScore[0] - 10 * i);
-  }
+    for (let i = 1; i <= acesCount; i++) {
+        dealerScore.push(dealerScore[0] - 10 * i);
+    }
 
-  let scores = dealerScore.filter((el) => el <= 21);
+    let scores = dealerScore.filter((el) => el <= 21);
 
-  $("#dealerScore").text(
-    `${scores.length < 1 ? Math.min(...dealerScore) : scores.join(" or ")}`
-  );
+    $("#dealerScore").text(
+        `${scores.length < 1 ? Math.min(...dealerScore) : scores.join(" or ")}`
+    );
 
-  if (Math.min(...dealerScore) > 21) {
-    setTimeout(() => {
-      alert("Dealer bust");
-      money += BET;
-      $("#money").text(money);
-    }, 1);
-    disableButtons();
-    return;
-  }
-  if (Math.min(...dealerScore) < 17 && first === false) {
-    setTimeout(() => {
-      dealerDraw();
-    }, 1000);
-  }
-  if (Math.min(...dealerScore) >= 17) {
-    setTimeout(() => {
-      alert(
-        `${
-          Math.min(...dealerScore) == playerScore
-            ? "Draw"
-            : Math.min(...dealerScore) < playerScore
-            ? "You win"
-            : "You lose"
-        }`
-      );
-    }, 1);
-    disableButtons();
-    Math.min(...dealerScore) == playerScore
-      ? (money += 0)
-      : Math.min(...dealerScore) < playerScore
-      ? (money += BET)
-      : money - +BET;
-    $("#money").text(money);
+    if (Math.min(...dealerScore) > 21) {
+        setTimeout(() => {
+            alert("Dealer bust");
+            money += BET;
+            $("#money").text(money);
+        }, 1);
+        disableButtons();
+        return;
+    }
+    if (Math.min(...dealerScore) < 17 && first === false) {
+        setTimeout(() => {
+            dealerDraw();
+        }, 1000);
+    }
+    if (Math.min(...dealerScore) >= 17) {
+        setTimeout(() => {
+            alert(
+                `${Math.min(...dealerScore) == playerScore
+                    ? "Draw"
+                    : Math.min(...dealerScore) < playerScore
+                        ? "You win"
+                        : "You lose"
+                }`
+            );
+        }, 1);
+        disableButtons();
+        Math.min(...dealerScore) == playerScore ?
+            (money += 0) :
+            Math.min(...dealerScore) < playerScore ?
+            (money += BET) :
+            money -= BET;
+        $("#money").text(money);
 
-    return;
-  }
+        return;
+    }
 };
 
 let hitBtn = document.getElementById("hit");
 hitBtn.addEventListener("click", hit);
 
 const resetGame = () => {
-  $("#playerCards").empty();
-  $("#dealerCards").empty();
-  resetCards();
-  userCards = [];
-  userScore = 0;
-  dealerCards = [];
-  dealerScore = 0;
-  $("#userScore").text(userScore);
-  $("#dealerScore").text(dealerScore);
-  $("#money").text(money);
+    $("#playerCards").empty();
+    $("#dealerCards").empty();
+    resetCards();
+    userCards = [];
+    userScore = 0;
+    dealerCards = [];
+    dealerScore = 0;
+    $("#userScore").text(userScore);
+    $("#dealerScore").text(dealerScore);
+    $("#money").text(money);
 
-  $("#hit").prop("disabled", false);
-  $("#stand").prop("disabled", false);
-  dealerDraw(true);
-  hit();
-  hit();
+    $("#hit").prop("disabled", false);
+    $("#stand").prop("disabled", false);
+    dealerDraw(true);
+    hit();
+    hit();
 };
 
 let resetBtn = document.getElementById("reset");
 resetBtn.addEventListener("click", resetGame);
 
 const stand = () => {
-  $("#hit").prop("disabled", true);
-  $("#stand").prop("disabled", true);
-  dealerDraw(false);
+    $("#hit").prop("disabled", true);
+    $("#stand").prop("disabled", true);
+    $("#userScore").text(playerScore);
+    dealerDraw(false);
 };
 
 let standBtn = document.getElementById("stand");
